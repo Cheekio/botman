d71c80a05d66e6ae6b823fc721d3de26c3938007
Cheekio <Timothy.J.Clifford@gmail.com>
2013-10-29 03:45:22 +0000

    Fixed call

--- plugins/rebase.py
+++ plugins/rebase.py
@@ -4,5 +4,5 @@ from util import hook
 @hook.command
 @hook.singlethread
 def rebase(inp):
-    call("git", "pull", "origin", "master")
+    call("git pull origin master", shell=True)
     return "Well, if that's going to work, then it did."
