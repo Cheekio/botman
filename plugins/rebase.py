eca1c223c196f94d75bdec1d9ce55e1b0bd5f748
Cheekio <Timothy.J.Clifford@gmail.com>
2013-10-29 03:43:18 +0000

    Hopefully fixed the call

--- plugins/rebase.py
+++ plugins/rebase.py
@@ -2,6 +2,7 @@ from subprocess import call
 from util import hook
 
 @hook.command
+@hook.singlethread
 def rebase(inp):
-    call("git pull origin master")
+    call("git", "pull", "origin", "master")
     return "Well, if that's going to work, then it did."
