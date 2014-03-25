c98a60b3b19f43ff110cf215235d7dae76f31e13
Cheekio <Timothy.J.Clifford@gmail.com>
2014-03-24 20:14:50 -0400

    Update vegfest.py

--- plugins/vegfest.py
+++ plugins/vegfest.py
@@ -5,5 +5,4 @@ from util import http, hook
 def vegfest(inp, say=None):
     ".vegfest -- gets current number of facebook attendees for the 2014 Worcester VegFest"
     data = http.get_json("https://worcester-vegfest-attendees.herokuapp.com/api")
-    return data
-    say("Worcester VegFest has " + data['number_of_event_attendees'] + " attendees on facebook.")
+    return "Worcester VegFest has " + data['number_of_event_attendees'] + " attendees on facebook."
