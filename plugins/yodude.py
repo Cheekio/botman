112a1b4780619fd362930f62c71023a497a287fc
Cheekio <Timothy.J.Clifford@gmail.com>
2014-07-11 15:04:49 -0400

    Update yodude.py

--- plugins/yodude.py
+++ plugins/yodude.py
@@ -1,14 +1,13 @@
 # yodude yos people
 
-from util import hook#, http
-#import requests
+from util import hook, http
+import requests
 
 @hook.command
 def yodude(inp):
-    return "Test Works: {0}".format(inp)
-#    '''.yodude <username> -- it will yo someone, dude'''
-#    r = requests.post('http://yofor.me/' + inp + '/BOTMAN')
-#    if r.status_code == 200:
-#        return '''yo successful'''
-#    else:
-#       return '''yo failed'''
+    '''.yodude <username> -- it will yo someone, dude'''
+    r = requests.post('http://yofor.me/' + inp + '/BOTMAN')
+    if r.status_code == 200:
+      '''yo successful'''
+    else:
+      '''yo failed'''
